d681a40a6bd6b48a59dc1a5e99ab5fb443dee49f
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-08-07 00:25:41 +0530

    feat : Varible testing

---
+++ Chapter 1/variables_test.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+    int a = 12;
+    float b = 12.45;
+    char c = 'c';
+    double d = 233423423;
+
+    printf("The interger number is :- %d \n",a);
+    printf("The float number is :- %f \n",b);
+    printf("The Character is :- %c \n", c);
+    printf("The double number is :- %lf \n" , d);
+    return 0 ;
+}
