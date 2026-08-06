44956e140d677d2c6306cc4fd9cbaa030f7ba7e2
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-08-06 23:46:28 +0530

    feat : Area of a sqaure

---
+++ Chapter 1/Area.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(){
+    int side ;
+    printf("\nEnter the length of side of the square \n");
+    scanf("%d", &side);
+
+    printf("The Area of the Square id :- %d \n",side * side);
+    return 0 ;
+}
