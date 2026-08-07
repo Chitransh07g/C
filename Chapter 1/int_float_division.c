22eaad857967d574a0a24f29709e767e2b7eb22a
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-08-07 20:08:54 +0530

    Solved problems

---
+++ Chapter 1/int_float_division.c
@@ -0,0 +1,8 @@
+# include<stdio.h>
+
+int main(){
+    int x = 7, y = 2;
+    printf("Interger division : %d \n", x/y);
+    printf("Float Division : %f  \n", (float)x / (float)y );
+    return 0;
+}
