375c8260ab7c59153a412150b149243be9ab29fb
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-08-15 18:16:40 +0530

    Instruction

---
+++ Chapter_2/Instruction.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include <math.h>
+// declaration 
+int main(){
+    int a = 22;
+    int b = a;
+    int c = b * 6;
+
+    int d = 1 , e;
+    int oldage  = 22;
+    // Arithimatic instructon 
+
+    int s = 1 , d = 2;
+
+    int sum = a + b;
+    int multiply = a * b ;
+    int z = pow(b,c);
+    return 0 ;
+}
