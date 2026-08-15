d875dd84c9f04edc25d7e0c70d80a86bc580c692
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-08-15 18:25:59 +0530

    learn : Understanding booleans

---
+++ Chapter_2/q1_relational.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main (){
+    int a = 5, b = 10;
+    printf("%d \n", a > b);
+    // here it gives the output 0 
+    // because the condition is false 
+    return 0;
+}
